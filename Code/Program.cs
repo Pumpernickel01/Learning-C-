2af7a47e025ecd8d3f3bff4e43b3c8f62b6fc94f
Paulius Budreika <paulius.budreika0@gmail.com>
2024-07-16 21:58:16 +0300

    More learning

--- Code/Program.cs
+++ Code/Program.cs
@@ -6,44 +6,69 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!"); // Write to the console (string)
+        // Console.WriteLine("Hello, World!"); // Write to the console (string)
 
-        Console.Beep(); // Make a beep sound
+        // Console.Beep(); // Make a beep sound
 
-        // This is a basic comment
+        // // This is a basic comment
 
-        /*
-            This is a multi-line comment
-        */
+        // /*
+        //     This is a multi-line comment
+        // */
 
-        Console.WriteLine("\tHello!"); // How to write escape sequences
+        // Console.WriteLine("\tHello!"); // How to write escape sequences
 
-        Console.ReadKey(); // Wait for a key press
+        // //Console.ReadKey(); // Wait for a key press
 
-        int x = 5; // Declare an integer variable
-        int y; // Declare an integer variable
-        y = 10; // Assign a value to a variable
+        // int x = 5; // Declare an integer variable
+        // int y; // Declare an integer variable
+        // y = 10; // Assign a value to a variable
 
-        int z = x + y; // Add two variables together
+        // int z = x + y; // Add two variables together
 
-        Console.WriteLine(z); // Write the value of a variable to the console
+        // Console.WriteLine(z); // Write the value of a variable to the console
 
-        int age = 18; // Declare an integer variable
+        // int age = 18; // Declare an integer variable
 
-        Console.WriteLine("Your age is: " + age); // Concatenate a string with a variable
+        // Console.WriteLine("Your age is: " + age); // Concatenate a string with a variable
 
-        double pi = 3.14159; // Declare a double variable
+        // //double pi = 3.14159; // Declare a double variable
 
-        bool isTrue = true; // Declare a boolean variable
+        // bool isTrue = true; // Declare a boolean variable
+        // Console.WriteLine(isTrue); // Write the value of a variable to the console
 
-        char letter = 'A'; // Declare a character 
+        // char letter = 'A'; // Declare a character 
         
-        string name = "John"; // Declare a string variable
+        // string name = "John"; // Declare a string variable
 
-        string userName = name + letter; // Concatenate a string with a character
+        // string userName = name + letter; // Concatenate a string with a character
+        // Console.WriteLine(userName); // Write the value of a variable to the console
 
-        Console.WriteLine(userName); // Write the value of a variable to the console
+
+        // // Constants are immutable values that are known at compile time and do not change for the life of the program
+        // const double pi = 3.14159; // Declare a double variable
+        // //pi = 420;
+        // Console.WriteLine(pi); // Write the value of a variable to the console
+
+        
+        // // Type casting are used to convert a variable from one data type to another data type
+        // double myDouble = 3.14; // Declare a double variable
+        // int b = Convert.ToInt32(myDouble); // Convert a double to an integer
+        // int myInt = (int)myDouble; // Cast a double to an integer
+
+        // Console.WriteLine(b); // Write the value of a variable to the console
+        // Console.WriteLine(myInt); // Write the value of a variable to the console
+        // Console.WriteLine(myInt.GetType()); // Write the value of a variable to the console and get the type of the variable
+
+        // int c = 10; // Declare an integer variable
+        // string f = Convert.ToString(c); // Convert an integer to a string
+        // Console.WriteLine(f); // Write the value of a variable to the console
+        // Console.WriteLine(f.GetType()); // Write the value of a variable to the console and get the type of the variable
 
 
+        //User input
+        Console.WriteLine("Enter your name: "); // Write to the console
+        string name = Console.ReadLine(); // Read a line of text from the console
+        System.Console.WriteLine("Hello, " + name); // Concatenate a string with a variable
     }
 }
--- Code/obj/Debug/net8.0/Code.AssemblyInfo.cs
+++ Code/obj/Debug/net8.0/Code.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ed557f51cd8b5b52861a97e86bd5a0ba88d786f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eea0c5932fb25c3acad9ccaac373efa8ad667217")]
 [assembly: System.Reflection.AssemblyProductAttribute("Code")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
