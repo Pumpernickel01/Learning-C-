c4529f0c2dcf1ca14dd6e15a1660f5a0ca07f7b8
Paulius Budreika <paulius.budreika0@gmail.com>
2024-07-16 16:05:26 +0300

    Updated

--- Code/obj/Debug/net8.0/Code.AssemblyInfo.cs
+++ Code/obj/Debug/net8.0/Code.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+109f2309f8e494b4817eed1e4a672e941a54186e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Code")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
